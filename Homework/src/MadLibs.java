1fb5eeba818ca65352c462f0c1090d211691d25e
Amara <amara_prather26@milton.edu>
2025-11-14 14:38:10 -0500

    initial commit

---
+++ Homework/src/MadLibs.java
@@ -0,0 +1,10 @@
+public class MadLibs {
+
+    public static void main(String[] args) {
+        MadLibs myStory = new Madlibs();
+    }
+
+    public Madlib(){
+        System.out.println("word");
+    }
+}
