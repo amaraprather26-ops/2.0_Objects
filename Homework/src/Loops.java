211df3803c808b9473239ede21fe6dc5c4430ea9
Amara <amara_prather26@milton.edu>
2026-01-12 16:46:41 -0500

    Finished Loops homework

---
+++ Homework/src/Loops.java
@@ -0,0 +1,42 @@
+public class Loops {
+    public static void main(String[] args) {
+        Loops myCount = new Loops();
+
+    }
+    public Loops (){
+        countUp();
+        countByThrees();
+        countDown();
+        nestedLoop();
+    }
+    public void countUp(){
+        for(int x = 1; x<6; x=x+1) {
+            System.out.println(x);
+        }
+    }
+
+    public void countByThrees(){
+        for(int x = 3; x<16; x=x+3){
+            System.out.print(x+" ");
+        }
+    }
+
+    public void countDown(){
+        System.out.println();
+        for(int x=10; x>0; x=x-1){
+            System.out.print(x+", ");
+        }
+        System.out.println();
+        System.out.println("Happy Loop Year!");
+    }
+
+    public void nestedLoop(){
+        for(int x=1; x<6; x=x+1){
+            for(int y=x; y>0; y=y-1){
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
